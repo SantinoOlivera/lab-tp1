35f63287bc7aa69a706fcdc1890d29a8d9c7e392
Tais <tais.cerda@est.fi.uncoma.edu.ar>
2026-08-27 17:12:28 -0300

    modified

--- patronVISITOR/CalcularSueldoVisitor.java
+++ patronVISITOR/CalcularSueldoVisitor.java
@@ -7,16 +7,16 @@ public class CalcularSueldoVisitor implements Visitor {
 
     public void visitar(EmpleadoComun empleado) {
         double sueldo  = empleado.getSueldo() + empleado.getAntiguedad() * 30000;
-        System.out.println(empleado.getNombre() + "cobra $" + sueldo);
+        System.out.println(empleado.getNombre() + " cobra $" + sueldo);
     }
 
     public void visitar(Pasante pasante) {
         double sueldo = pasante.getHoras() * pasante.getValorHora();
-        System.out.println(pasante.getNombre() + "cobra $" + sueldo);
+        System.out.println(pasante.getNombre() + " cobra $" + sueldo);
     }
 
     public void visitar(Secretario secretario) {
         double sueldo = secretario.getSueldo() + secretario.getHorasExtra() * 10000;
-        System.out.println(secretario.getNombre() + "cobra $" + sueldo);
+        System.out.println(secretario.getNombre() + " cobra $" + sueldo);
     }
 }
